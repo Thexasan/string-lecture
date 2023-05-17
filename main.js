07ced761869a6986e0b2ba45c96fc78352687c6f
Thexasan <129182612+Thexasan@users.noreply.github.com>
2023-05-17 15:26:15 +0500

    first

---
+++ main.js
@@ -0,0 +1,52 @@
+// let str = 'abubakr'
+// let res = str.at(0).toUpperCase()+str.substring(1)
+// console.log(res)
+
+
+        //----------------
+// function give(str){
+//     let a = 'something'.concat(' ',str)
+//     return a
+// }
+// console.log(give('is beter than'))
+
+//2
+// function n(str){
+//     return +str
+// }
+// console.log(typeof(n('55')))
+
+//3
+// function greet(str){
+//     if(str.includes('Mubashir')){
+//         return 'Hello, my love!'
+//     }
+//     else{
+//         return `Hello, ${str}`
+//     }
+// }
+// console.log(greet('Mubashir'))
+
+//4
+// function bool(n){
+//     return n.toString(n)
+// }
+// console.log(typeof(bool(true)))
+
+//5
+// function comp(str1,str2){
+//     return str1.split('').length==str2.split('').length
+// }
+// console.log(comp('HB','CD'))
+
+//6
+// function len(str,cnt=0) {
+//     if (str =='') {
+//       return 0;
+//     } 
+//       return len(str.slice(1))+ ++cnt
+// }
+// console.log(len('apple')); 
+// console.log(len('make'));
+// console.log(len('')); 
+
